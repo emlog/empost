3227919cc8490439fb5207bbf465c69be817e988
ljq2731 <ljq2731@163.com>
2022-09-16 16:12:48 +0800

    新增右键发布笔记或文章，暂时支持选中文字直接发布笔记，后续支持右键弹出笔记和文章页面。

--- background.js
+++ background.js
@@ -1,6 +1,91 @@
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    console.log("Request comes from content script " + sender.tab.id);
-    if (request.greeting === "close_tab"){
+import md5 from './md5.js';
+/**
+ * 用于接收关闭配置页的方法，可以关闭当前tab页
+ */
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  console.log('Request comes from content script ' + sender.tab.id);
+  if (request.greeting === 'close_tab') {
     chrome.tabs.remove(sender.tab.id);
-    }
+  }
+});
+
+chrome.runtime.onInstalled.addListener(function () {
+  // 浏览器页面右键菜单逻辑
+  const menu = {
+    menus: [
+      {
+        id: 'emloger',
+        visible: true,
+        title: 'emlog发布插件',
+        contexts: ['page'], // page表示页面右键就会有这个菜单，如果想要当选中文字时才会出现此右键菜单，用：selection
+      },
+      {
+        id: 'postnote',
+        visible: true,
+        parentId: 'emloger',
+        title: '发布笔记',
+        contexts: ['page'],
+      },
+      {
+        id: 'postnoteselect',
+        visible: true,
+        title: '选中文字发布笔记',
+        contexts: ['selection'],
+      },
+      {
+        id: 'postarticle',
+        visible: true,
+        parentId: 'emloger',
+        title: '发布文章',
+        contexts: ['page'],
+      },
+    ],
+  };
+
+  const createMenu = () => {
+    menu.menus.forEach((value) => {
+      chrome.contextMenus.create(value);
     });
+  };
+  createMenu();
+});
+
+chrome.contextMenus.onClicked.addListener(function callback(param) {
+  console.log(param);
+  switch (param.menuItemId) {
+    case 'postnote': // 发布笔记
+    console.log('发布笔记页面弹出');
+      break;
+    case 'postarticle': // 发布文章
+      console.log('发布文章页面弹出');
+      break;
+    case 'postnoteselect': // 右键选中文字直接发布笔记
+      console.log('选中文字直接发布笔记');
+      var apikey = '';
+      var apiurl = '';
+      chrome.storage.local.get(['postblogtoken', 'posturl'], function (result) {
+        if (result.postblogtoken && result.posturl) {
+          apikey = result.postblogtoken;
+          apiurl = result.posturl;
+          let req_time = new Date().getTime();
+          // 计算 accesstoken
+          let req_sign = md5(req_time + '' + apikey);
+
+          fetch(apiurl + '/?rest-api=note_post', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            body:
+              'req_time=' + req_time + '&req_sign=' + req_sign + '&t=' + param.selectionText,
+          }).then(function (result) {
+            // 待完善
+            console.log('发布笔记完成弹窗');
+          });
+        }
+      });
+      break;
+    default:
+      break;
+  }
+});
--- custom.js
+++ custom.js
@@ -33,32 +33,10 @@ $(function () {
       chrome.runtime.openOptionsPage(function(){})
       return;
     }
-     // todo 计算 accesstoken
+     // 计算 accesstoken
     let req_sign = md5(req_time +''+ apikey);
     if ($('#title').val() !== '' && $('#content').val() !== '') {
-      $.ajax({
-        url: apiurl+'/?rest-api=article_post',
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/x-www-form-urlencoded',
-        },
-        async: true,
-        data:
-          'req_time=' +
-          req_time + '&req_sign='+req_sign+
-          '&title=' +
-          $('#title').val() +
-          '&content=' +
-          $('#content').val() +
-          '&excerpt=' +
-          $('#abstrat').val(),
-        success: function (result) {
-          alert('发布成功');
-          $('#title').val('');
-          $('#abstrat').val('');
-          $('#content').val('');
-        },
-      });
+      postarticle(apiurl, req_time, req_sign);
     } else {
       alert('请填写内容后再发布。');
     }
@@ -66,34 +44,73 @@ $(function () {
 
    // 发布笔记
    $('#postnote').on('click', function () {
-    let req_time = new Date().getTime();
     if('' == apikey) {
       alert('请填写url和apikey');
       chrome.runtime.openOptionsPage(function(){})
       return;
     }
-     // todo 计算 accesstoken
-    let req_sign = md5(req_time +''+ apikey);
     if ($('#note').val() !== '') {
-      $.ajax({
-        url: apiurl+'/?rest-api=note_post',
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/x-www-form-urlencoded',
-        },
-        async: true,
-        data:
-          'req_time=' +
-          req_time + '&req_sign='+req_sign+
-          '&t=' +
-          $('#note').val() ,
-        success: function (result) {
-          alert('发布成功');
-          $('#note').val('');
-        },
-      });
+      postnote($('#note').val());
     } else {
       alert('请填写内容后再发布。');
     }
   });
+  /**
+ * 发布文章
+ * @param {string} apiurl 
+ * @param {string} req_time 
+ * @param {string} req_sign 
+ */
+function postarticle(apiurl, req_time, req_sign) {
+  $.ajax({
+    url: apiurl + '/?rest-api=article_post',
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    async: true,
+    data: 'req_time=' +
+      req_time + '&req_sign=' + req_sign +
+      '&title=' +
+      $('#title').val() +
+      '&content=' +
+      $('#content').val() +
+      '&excerpt=' +
+      $('#abstrat').val(),
+    success: function (result) {
+      alert('发布成功');
+      $('#title').val('');
+      $('#abstrat').val('');
+      $('#content').val('');
+    },
+  });
+}
+
+/**
+ * 发布笔记
+ * @param {string} apiurl url参数
+ * @param {string} req_time 时间戳
+ * @param {string} req_sign 签名
+ * @param {string} note 笔记内容
+ */
+function postnote(note) {
+  let req_time = new Date().getTime();
+     // 计算 accesstoken
+  let req_sign = md5(req_time +''+ apikey);
+  $.ajax({
+    url: apiurl + '/?rest-api=note_post',
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    async: true,
+    data: 'req_time=' +
+      req_time + '&req_sign=' + req_sign +
+      '&t=' + note,
+    success: function (result) {
+      alert('发布成功');
+      $('#note').val('');
+    },
+  });
+}
 });
