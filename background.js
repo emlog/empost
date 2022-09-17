af9976728a0534b6ef3e284ff1d771ad782d28fc
ljq2731 <ljq2731@163.com>
2022-09-17 13:41:44 +0800

    增加右键选中文字后发布文章和笔记功能

--- background.js
+++ background.js
@@ -13,19 +13,19 @@ chrome.runtime.onInstalled.addListener(function () {
   // 浏览器页面右键菜单逻辑
   const menu = {
     menus: [
-      {
-        id: 'emloger',
-        visible: true,
-        title: 'emlog发布插件',
-        contexts: ['page'], // page表示页面右键就会有这个菜单，如果想要当选中文字时才会出现此右键菜单，用：selection
-      },
-      {
-        id: 'postnote',
-        visible: true,
-        parentId: 'emloger',
-        title: '发布笔记',
-        contexts: ['page'],
-      },
+      // {
+      //   id: 'emloger',
+      //   visible: true,
+      //   title: 'emlog发布插件',
+      //   contexts: ['page'], // page表示页面右键就会有这个菜单，如果想要当选中文字时才会出现此右键菜单，用：selection
+      // },
+      // {
+      //   id: 'postnote',
+      //   visible: true,
+      //   parentId: 'emloger',
+      //   title: '发布笔记',
+      //   contexts: ['page'],
+      // },
       {
         id: 'postnoteselect',
         visible: true,
@@ -33,11 +33,10 @@ chrome.runtime.onInstalled.addListener(function () {
         contexts: ['selection'],
       },
       {
-        id: 'postarticle',
+        id: 'postarticleselect',
         visible: true,
-        parentId: 'emloger',
-        title: '发布文章',
-        contexts: ['page'],
+        title: '选中文字发布文章',
+        contexts: ['selection'],
       },
     ],
   };
@@ -53,39 +52,47 @@ chrome.runtime.onInstalled.addListener(function () {
 chrome.contextMenus.onClicked.addListener(function callback(param) {
   console.log(param);
   switch (param.menuItemId) {
-    case 'postnote': // 发布笔记
-    console.log('发布笔记页面弹出');
+    case 'postarticleselect': // 选中文字右键直接发布文章
+      console.log('选中文字直接发布文章');
+      selectTextPost(param,'/?rest-api=article_post');
       break;
-    case 'postarticle': // 发布文章
-      console.log('发布文章页面弹出');
-      break;
-    case 'postnoteselect': // 右键选中文字直接发布笔记
+    case 'postnoteselect': // 选中文字右键直接发布笔记
       console.log('选中文字直接发布笔记');
-      var apikey = '';
-      var apiurl = '';
-      chrome.storage.local.get(['postblogtoken', 'posturl'], function (result) {
-        if (result.postblogtoken && result.posturl) {
-          apikey = result.postblogtoken;
-          apiurl = result.posturl;
-          let req_time = new Date().getTime();
-          // 计算 accesstoken
-          let req_sign = md5(req_time + '' + apikey);
-
-          fetch(apiurl + '/?rest-api=note_post', {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/x-www-form-urlencoded',
-            },
-            body:
-              'req_time=' + req_time + '&req_sign=' + req_sign + '&t=' + param.selectionText,
-          }).then(function (result) {
-            // 待完善
-            console.log('发布笔记完成弹窗');
-          });
-        }
-      });
+      selectTextPost(param,'/?rest-api=note_post');
       break;
     default:
       break;
   }
 });
+// 选中文字发布 笔记或者文章。 文章标题截取前十个字
+/**
+ * 选中文字发布 笔记或者文章。 文章标题截取前十个字
+ * @param {*} param 右键获取参数 selection是选中文字
+ * @param {string} urlpath 发布接口路径
+ */
+function selectTextPost(param,urlpath) {
+  var apikey = '';
+  var apiurl = '';
+  chrome.storage.local.get(['postblogtoken', 'posturl'], function (result) {
+    if (result.postblogtoken && result.posturl) {
+      apikey = result.postblogtoken;
+      apiurl = result.posturl;
+      let req_time = new Date().getTime();
+      // 计算 accesstoken
+      let req_sign = md5(req_time + '' + apikey);
+
+      fetch(apiurl + urlpath, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: 'req_time=' + req_time + '&req_sign=' + req_sign + '&t=' + param.selectionText +
+        '&title=' + param.selectionText.substr(0,20) + '&content=' + param.selectionText + '&excerpt=' + param.selectionText.substr(0,40),
+      }).then(function (result) {
+        // 待完善 background 不能使用alert。因为没有window对象、
+        console.log('发布完成弹窗');
+      });
+    }
+  });
+}
+
