6173edc119ba31b7f3836efe9aacb10e3a7f7f2a
ljq2731 <ljq2731@163.com>
2022-09-20 22:50:33 +0800

    完善发布文章的标题获取方式-采用浏览器标题

--- background.js
+++ background.js
@@ -13,12 +13,12 @@ chrome.runtime.onInstalled.addListener(function () {
   // 浏览器页面右键菜单逻辑
   const menu = {
     menus: [
-      {
-        id: 'emloger',
-        visible: true,
-        title: 'emlog发布插件',
-        contexts: ['page'], // page表示页面右键就会有这个菜单，如果想要当选中文字时才会出现此右键菜单，用：selection
-      },
+      // {
+      //   id: 'emloger',
+      //   visible: true,
+      //   title: 'emlog发布插件',
+      //   contexts: ['page'], // page表示页面右键就会有这个菜单，如果想要当选中文字时才会出现此右键菜单，用：selection
+      // },
       // {
       //   id: 'postnote',
       //   visible: true,
@@ -72,9 +72,8 @@ function openEditor() {
   // 发布成功弹窗
   postSucces();
 }
-// 选中文字发布 笔记或者文章。 文章标题截取前十个字
 /**
- * 选中文字发布 笔记或者文章。 文章标题截取前十个字
+ * 选中文字发布 笔记或者文章。 文章标题 取浏览器标题
  * @param {*} param 右键获取参数 selection是选中文字
  * @param {string} urlpath 发布接口路径
  */
@@ -88,18 +87,25 @@ function selectTextPost(param,urlpath) {
       let req_time = new Date().getTime();
       // 计算 accesstoken
       let req_sign = md5(req_time + '' + apikey);
-
+      chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+      console.log(tabs[0]); // 取得浏览器标题作为文章标题
       fetch(apiurl + urlpath, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/x-www-form-urlencoded',
         },
         body: 'req_time=' + req_time + '&req_sign=' + req_sign + '&t=' + param.selectionText +
-        '&title=' + param.selectionText.substr(0,20) + '&content=' + param.selectionText + '&excerpt=' + param.selectionText.substr(0,40),
+        '&title=' + tabs[0].title + '&content=' + param.selectionText + '&excerpt=' + param.selectionText.substr(0,40),
       }).then(function (result) {
         // 发布成功的弹窗
           postSucces();
-      });
+      });});
+    } else {
+      // 如果参数未获取到-重新填写apikey及url
+      // 打开设置页
+      chrome.runtime.openOptionsPage(function () {
+        console.log();
+      })
     }
   });
 }
@@ -110,7 +116,7 @@ function postSucces() {
   chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
   chrome.scripting.insertCSS({
     target: { tabId: tabs[0].id },
-    files: ['./layer/theme/default/layer.css',],
+    files: ['./layer/theme/default/layer.css'],
   });
   chrome.scripting.executeScript({
     target: { tabId: tabs[0].id },
