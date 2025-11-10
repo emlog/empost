1b3f8d0e8c7342d6d7eef7276d2d5b628855d5a9
snow <4344235+emlog@users.noreply.github.com>
2025-11-10 15:10:40 +0800

    up

--- pop/custom.js
+++ pop/custom.js
@@ -1,58 +1,44 @@
 $(function () {
-    let apikey = '';
-    let apiurl = '';
-    chrome.storage.local.get(['postblogtoken', 'posturl'], function (result) {
-        if (result.postblogtoken && result.posturl) {
-            apikey = result.postblogtoken;
-            apiurl = result.posturl;
-        } else {
-            layer.alert('请填写url和apikey');
-            chrome.runtime.openOptionsPage(function () {
-            })
-        }
-    });
-
     // 重置
     $('#reset').on('click', function () {
         $('#title').val('');
         $('#abstrat').val('');
         $('#content').val('');
     });
-     // 粘贴监听 可以在此处增加版权信息外链链接等
-    // document.addEventListener("paste", function (e) {  // 粘贴事件触发
-    //     navigator.clipboard.readText().then(
-    //         clipText => {
-    //             document.querySelector("#content").innerText = clipText
-    //         });;
-    // }, false);
+
     // 发布文章
     $('#post').on('click', function () {
-        let req_time = new Date().getTime();
-        if ('' === apikey) {
-            layer.alert('请填写url和apikey');
-            chrome.runtime.openOptionsPage(function () {
-            })
-            return;
-        }
-        // todo 计算 accesstoken
-        let req_sign = md5(req_time + '' + apikey);
-        if ($('#title').val() === '' || $('#content').val() === '') {
-            layer.alert('请填写内容后再发布。');
-        }
-        $.ajax({
-            url: apiurl + '/?rest-api=article_post',
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded',
-            },
-            async: true,
-            data: 'req_time=' + req_time + '&req_sign=' + req_sign + '&title=' + encodeURIComponent($('#title').val()) + '&content=' + encodeURIComponent($('#content').val()) + '&excerpt=' + encodeURIComponent($('#abstrat').val()),
-            success: function (result) {
-                layer.alert('文章发布成功');
-                $('#title').val('');
-                $('#abstrat').val('');
-                $('#content').val('');
-            },
+        chrome.storage.local.get(['postblogtoken', 'posturl'], function (result) {
+            if (result.postblogtoken && result.posturl) {
+                let apikey = result.postblogtoken;
+                let apiurl = result.posturl;
+                let req_time = new Date().getTime();
+                let req_sign = md5(req_time + '' + apikey);
+
+                if ($('#title').val() === '' || $('#content').val() === '') {
+                    layer.alert('请填写内容后再发布。');
+                    return;
+                }
+
+                $.ajax({
+                    url: apiurl + '/?rest-api=article_post',
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/x-www-form-urlencoded',
+                    },
+                    async: true,
+                    data: 'req_time=' + req_time + '&req_sign=' + req_sign + '&title=' + encodeURIComponent($('#title').val()) + '&content=' + encodeURIComponent($('#content').val()) + '&excerpt=' + encodeURIComponent($('#abstrat').val()),
+                    success: function (result) {
+                        layer.alert('文章发布成功');
+                        $('#title').val('');
+                        $('#abstrat').val('');
+                        $('#content').val('');
+                    },
+                });
+            } else {
+                layer.alert('请填写url和apikey');
+                chrome.runtime.openOptionsPage(function () {});
+            }
         });
     });
 
@@ -63,25 +49,35 @@ $(function () {
 
     // 发布笔记
     $('#post_note').on('click', function () {
-        let req_time = new Date().getTime();
-        if ('' === apikey) {
-            layer.alert('请填写url和apikey');
-            chrome.runtime.openOptionsPage(function () {
-            })
-            return;
-        }
-        if ($('#content').val() === '') {
-            layer.alert('请填写内容后再发布。');
-        }
-        let req_sign = md5(req_time + '' + apikey);
-        $.ajax({
-            url: apiurl + '/?rest-api=note_post ', method: 'POST', headers: {
-                'Content-Type': 'application/x-www-form-urlencoded',
-            }, async: true, data: 'req_time=' + req_time + '&req_sign=' + req_sign + '&t=' + $('#content').val(), success: function (result) {
-                layer.alert('笔记发布成功');
-                $('#content').val('');
-            },
-        });
+        chrome.storage.local.get(['postblogtoken', 'posturl'], function (result) {
+            if (result.postblogtoken && result.posturl) {
+                let apikey = result.postblogtoken;
+                let apiurl = result.posturl;
+                let req_time = new Date().getTime();
 
+                if ($('#content').val() === '') {
+                    layer.alert('请填写内容后再发布。');
+                    return;
+                }
+
+                let req_sign = md5(req_time + '' + apikey);
+                $.ajax({
+                    url: apiurl + '/?rest-api=note_post',
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/x-www-form-urlencoded',
+                    },
+                    async: true,
+                    data: 'req_time=' + req_time + '&req_sign=' + req_sign + '&t=' + encodeURIComponent($('#content').val()),
+                    success: function (result) {
+                        layer.alert('笔记发布成功');
+                        $('#content').val('');
+                    },
+                });
+            } else {
+                layer.alert('请填写url和apikey');
+                chrome.runtime.openOptionsPage(function () {});
+            }
+        });
     });
 });
